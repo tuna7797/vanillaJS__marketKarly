f4ae306a7532f0551fef858b1dade8bbc300656a
jjang-aaa <jadoit473@gmail.com>
2023-07-31 14:16:41 +0900

    feat: 구매수량 증감하는 기능 구현
    + 버튼, - 버튼을 누르면 구매수량이 증감하는 기능을 추가함

---
+++ client/js/productDetail.js
@@ -0,0 +1,51 @@
+import {
+  attr,
+  bindEvent,
+  clearContents,
+  getNode,
+  insertLast,
+} from "../lib/index.js";
+
+// 상품 선택 카운트 기능
+const selectCount = getNode(".product_detail_select_count");
+
+const plusButton = getNode(".product_detail_plus");
+
+const minusButton = getNode(".product_detail_minus");
+
+const productTotalPrice = getNode(".product_detail_total_price");
+
+// + 버튼을 누르면 카운트값이 증가하는 기능
+function handlerPlus(e) {
+  e.preventDefault();
+
+  countPlusNumber();
+}
+
+function countPlusNumber() {
+  let countNumber = +selectCount.innerText + 1;
+  if (countNumber === 0) {
+    return alert("최소 주문수량은 1 입니다.");
+  }
+  clearContents(selectCount);
+  insertLast(selectCount, `${countNumber}`);
+}
+
+bindEvent(plusButton, "click", handlerPlus);
+
+// - 버튼을 누르면 값이 카운트값이 감소하는 기능
+function handlerMinus(e) {
+  e.preventDefault();
+  countMiusNumber();
+}
+
+function countMiusNumber() {
+  let countNumber = +selectCount.innerText - 1;
+  if (countNumber === 0) {
+    return alert("최소 주문수량은 1 입니다.");
+  }
+  clearContents(selectCount);
+  insertLast(selectCount, `${countNumber}`);
+}
+
+bindEvent(minusButton, "click", handlerMinus);
