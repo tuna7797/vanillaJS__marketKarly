1f51c410d6c898b6f90e43518cf56399aaf82e15
tuna7797 <gmlwjd7731@gmail.com>
2023-07-31 16:42:56 +0900

    feat : dropDown 추가 - 좌측 네비 dropDown 기능 추가 - data.json 수정

--- client/js/main.js
+++ client/js/main.js
@@ -9,4 +9,15 @@ const productData = response.data;
 
 productData.forEach((item)=> renderProductList(productWrap,item))
 
-console.log(productData);
+
+//brandList Drop Down
+
+const sideBarButton = $('.side_bar_button_clicked');
+
+sideBarButton.addEventListener('click', () => {
+  
+  const brandList = $('.brand');
+  brandList.classList.toggle("brand_list_hide");
+  $('.arrow_top').classList.toggle("arrow_top_rotate");
+
+});
